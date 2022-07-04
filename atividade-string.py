132fff1f57a09f32ade28e80b4b55dd0b12e3b2a
heiytor <107213601+heiytor@users.noreply.github.com>
2022-07-04 13:26:45 -0300

    Add files via upload

---
+++ atividade-string.py
@@ -0,0 +1,10 @@
+# Estruture três códigos, os quais devem conter uma função de manipulação de string que obtenha resultado.
+
+string1, string2, string3 = "ufpe", "HEITOR DANILO SILVA BARROS", "sofyex"
+
+print(string1)
+print(f"{string1.upper()}\n")
+print(string2)
+print(f"{string2.title()}\n")
+print(string3)
+print(f"{string3.replace('y', 't').capitalize()}")
