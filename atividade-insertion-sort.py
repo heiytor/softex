083eabadf328fa28c39cb2bc1dd6f8b269f601ad
heiytor <107213601+heiytor@users.noreply.github.com>
2022-07-05 01:53:09 -0300

    Add files via upload

---
+++ atividade-insertion-sort.py
@@ -0,0 +1,21 @@
+import random
+
+
+def insertionSort(list):
+    for i in range(1, len(list)):
+        key = list[i]
+        j = i - 1
+        while j >= 0 and key < list[j]:
+            list[j + 1] = list[j]
+            j = j - 1
+        list[j + 1] = key
+    return list
+
+
+lista = []
+for _, i in enumerate(range(30)):
+    lista.append(random.randint(1, 999))
+
+print(lista)
+print(insertionSort(lista))
+
